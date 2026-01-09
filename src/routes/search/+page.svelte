beecbac4e4d55abe6b0e184908507fa901425c97
Martin Faeroey <martin.faeroey@proton.me>
2026-01-09 08:26:36 +0100

    updated styling, allow for dark mode

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -20,7 +20,7 @@
 
 <label class="grid p-4">
 	<span>Search for gem</span>
-	<input type="text" bind:value={search} placeholder="e.g: Split arrow" class="border p-2" >
+	<input type="text" bind:value={search} placeholder="e.g: Split arrow" class="border rounded p-2 dark:border-white/25 border-black/25" >
 </label>
 <fieldset class="flex gap-4 px-4">
 	<legend class="border-b dark:border-white/25 border-black/25 dark:text-white/75 text-black/75 pb-2 mb-2">Filter by attributes:</legend>
