6836bade621f45de629ebcca3c0eda4ce71f0d72
Martin Faeroey <martin.faeroey@proton.me>
2026-01-10 17:28:51 +0100

    styling changes

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -7,7 +7,7 @@
 	let { label, checked = $bindable<boolean>(), labelTextClasses }: Props = $props();
 </script>
 
-<label class="flex justify-between gap-2 accent-sky-500">
-	<span class={labelTextClasses}>{label}</span>
+<label class="flex justify-between gap-2 dark:accent-white accent-black items-center text">
 	<input type="checkbox" bind:checked />
+	<span class={labelTextClasses}>{label}</span>
 </label>
